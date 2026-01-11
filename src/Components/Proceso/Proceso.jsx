d54cdbb5f6d621e318fb439d19f27d58c2597279
SebastianOso <sebastian.oso03@gmail.com>
2026-01-11 13:28:33 -0600

    feat: prueba

---
+++ src/Components/Proceso/Proceso.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Proceso = () => {
+  return (
+        <div
+            className="
+            bg-[#E3536B]
+            h-[616px]
+            w-[1300px]
+            [clip-path:polygon(0_0,100%_0,96%_100%,4%_100%)]
+            rounded-tr-4xl
+            "
+        >
+            Hola
+        </div>
+  )
+}
+
+export default Proceso
