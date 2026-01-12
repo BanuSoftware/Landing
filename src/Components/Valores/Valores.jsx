940c630864615009198a07a72370e6d4d524a36a
Enrique Andrey Ramírez Lira <kikin@Enriques-MacBook-Air.local>
2026-01-12 13:46:04 -0600

    fix: padding and responsive interface

--- src/Components/Valores/Valores.jsx
+++ src/Components/Valores/Valores.jsx
@@ -1,6 +1,8 @@
+// Values.jsx
 import Valores from "./components_valores/ContenidoValores";
 import iconMaybe from "../../Assets/Images/maybe 3.svg";
 import monoMusica from "../../Assets/Images/MonoMusica.svg";
+import texturaBackground from "../../Assets/Images/Textura.png";
 
 function Values() {
     const valores = [
@@ -58,39 +60,54 @@ function Values() {
     
     return (
         <div 
-            className="relative"
+            className="relative min-h-screen w-full flex items-center justify-center px-4 sm:px-6 lg:px-8"
             style={{
-                width: '1222px',
-                height: '574px',
-                marginTop: '205px',
-                marginLeft: '127px',
-                opacity: 1
+                backgroundImage: `url(${texturaBackground})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
             }}
         >
-            <div className="relative w-full h-full">
-                {/* Mono música */}
-                <img 
-                    src={monoMusica} 
-                    alt="Mono Música" 
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                />
-                
-                {/* Valores */}
-                {valores.map((valor) => (
-                    <div 
-                        key={valor.id} 
-                        className="absolute"
-                        style={{ top: valor.position.top, left: valor.position.left }}
-                    >
-                        <Valores 
-                            id={valor.id} 
-                            icon={valor.icon}
-                            title={valor.title} 
-                            content_left={valor.texts[0]} 
-                            content_right={valor.texts[1]}
-                        /> 
-                    </div>
-                ))}
+            <div 
+                className="relative"
+                style={{
+                    width: '1222px',
+                    height: '574px'
+                }}
+            >
+                <div className="relative w-full h-full">
+                    {/* Mono música - tamaño y posición absoluta fija */}
+                    <img 
+                        src={monoMusica} 
+                        alt="Mono Música" 
+                        style={{
+                            position: 'absolute',
+                            top: '287px',
+                            left: '611px',
+                            transform: 'translate(-50%, -50%)'
+                        }}
+                    />
+                    
+                    {/* Valores - posiciones absolutas fijas */}
+                    {valores.map((valor) => (
+                        <div 
+                            key={valor.id} 
+                            style={{ 
+                                position: 'absolute',
+                                top: valor.position.top,
+                                left: valor.position.left
+                            }}
+                        >
+                            <Valores 
+                                id={valor.id} 
+                                icon={valor.icon}
+                                title={valor.title} 
+                                content_left={valor.texts[0]} 
+                                content_right={valor.texts[1]}
+                            /> 
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/Components/Valores/components_valores/ContenidoValores.jsx
+++ src/Components/Valores/components_valores/ContenidoValores.jsx
@@ -1,3 +1,5 @@
+
+// ContenidoValores.jsx
 import defaultIcon from "../../../Assets/Images/maybe 3.svg";
 
 function Valores({title, content_left, content_right, icon}) {
