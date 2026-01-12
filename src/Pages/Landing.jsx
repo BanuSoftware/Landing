74df06adea254c188fd55b129b7009e51fff3dbd
Enrique Andrey Ramírez Lira <kikin@Enriques-MacBook-Air.local>
2026-01-11 21:47:09 -0600

    feat: mockup design in html

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,10 +1,16 @@
 import Carousel from "../Components/Servicios/Servicios.jsx";
+import Valores from "../Components/Valores/Valores.jsx";
 
 function Landing() {
   return (
-    <div className="min-h-screen bg-white">
-      <Carousel />
-    </div>
+    <>
+      <div className="min-h-screen bg-white">
+        <Carousel />
+      </div>
+      <div className="min-h-screen bg-white">
+        <Valores />
+      </div>
+    </>
   );
 }
 
