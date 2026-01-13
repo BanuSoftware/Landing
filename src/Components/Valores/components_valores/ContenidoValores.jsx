11494b745e891e2d925e888aba82b269d8dcde57
Enrique Andrey Ramírez Lira <kikin@Enriques-MacBook-Air.local>
2026-01-12 20:05:44 -0600

    feat: animation 2.0

--- src/Components/Valores/components_valores/ContenidoValores.jsx
+++ src/Components/Valores/components_valores/ContenidoValores.jsx
@@ -3,9 +3,9 @@ import defaultIcon from "../../../Assets/Images/maybe 3.svg";
 
 function Valores({title, content_left, content_right, icon}) {
   return (
-    <div className="group relative overflow-hidden rounded-2xl p-4 transition-all duration-500 hover:scale-105 hover:shadow-[0_0_20px_rgba(214,214,214,0.5)]">
+    <div className="group relative overflow-hidden rounded-2xl p-4 transition-all duration-500 hover:scale-105 ]">
       {/* Animación*/}
-      <div className="absolute -top-1/2 -left-1/2 w-[200%] h-[200%] bg-gradient-to-b from-transparent via-transparent to-gray-400/30 -rotate-45 opacity-0 transition-all duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-full pointer-events-none"></div>
+      <div className="absolute -top-1/2 -left-1/2 w-[200%] h-[200%] -rotate-45 opacity-0 transition-all duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-full pointer-events-none"></div>
       
       {/* Contenido */}
       <div className="relative z-10">
