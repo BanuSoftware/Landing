10b8403f376cf741e9901a4cf08889a7a365f649
AlexStryer <alex.stryer@gmail.com>
2026-01-12 21:05:40 -0600

    feat: seccion de Mision y Vision implementada con animacion del mono. Falta: Limpiar el codigo, documentarlo y hacer la animacion mas fluida.

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,8 +1,10 @@
+import VisionMision from "../Components/VisionMision/VisionMision.jsx";
 import Carousel from "../Components/Servicios/Servicios.jsx";
 
 function Landing() {
   return (
     <div className="min-h-screen bg-white">
+      <VisionMision />
       <Carousel />
     </div>
   );
